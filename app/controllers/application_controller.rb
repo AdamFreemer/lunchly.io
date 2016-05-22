bba49f30e615aad2fb1464b31e1a8d87f8482007
Adam Freemer <a@freemer.com>
2016-05-22 14:25:04 -0400

    Geocode test code update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 	def get_remote_ip
-		if request.remote_ip == '127.0.0.1' || '::1' ## for dev testing both IPv4 & IPv6
+		if request.remote_ip == '127.0.0.1' || request.remote_ip == '::1' ## for dev testing both IPv4 & IPv6
 		  @my_ip = '68.45.67.89'
 		else
 		  @my_ip = request.remote_ip
