e951e7837d68a3f5e4a6a757bc73c38778027b22
Adam Freemer <a@freemer.com>
2016-05-25 21:05:58 -0400

    Updated initializer for Cloud 66 deploy issue.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,2 +1,2 @@
 ### This is only for dev
-CONFIG_API = YAML.load_file("#{Rails.root.to_s}/config/keys.yml") if Rails.env.production?
+CONFIG_API = YAML.load_file("#{Rails.root.to_s}/config/keys.yml") if Rails.env.development?
